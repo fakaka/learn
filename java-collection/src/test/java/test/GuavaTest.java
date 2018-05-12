8b6d82373c797f0cb8d163876836b2ca36a3e8a5
hairpin <vsmj012@163.com>
2018-05-12 13:50:45 +0800

    Guava test

--- java-collection/src/test/java/test/GuavaTest.java
+++ java-collection/src/test/java/test/GuavaTest.java
@@ -1,21 +1,15 @@
 package test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 
+import com.google.common.base.CaseFormat;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
 
 public class GuavaTest {
-
-	public static void main(String[] args) {
-		ArrayList<String> newArrayList = Lists.newArrayList();
-		newArrayList.add("a");
-
-	}
 
 	@Test
 	public void strings() {
@@ -25,8 +19,7 @@ public class GuavaTest {
 	@Test
 	public void spiter() {
 		Splitter splitter = Splitter.on(",").trimResults().omitEmptyStrings();
-		Iterable<String> split = splitter.split("a,b,  c,,");
-		System.out.println(split);
+		System.out.println(splitter.split("a,b,  c,,"));
 
 	}
 
@@ -37,4 +30,15 @@ public class GuavaTest {
 		System.out.println(join);
 	}
 
+	@Test
+	public void testJoiner() {
+		System.out.println(Joiner.on(",").skipNulls().join(Arrays.asList(1, 2, 3, 4, 5, null, 6)));
+	}
+
+	@Test
+	public void testCaseFormat() {
+		System.out.println(CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, "test-data"));
+		System.out.println(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, "test_data"));
+		System.out.println(CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, "test_data"));
+	}
 }
